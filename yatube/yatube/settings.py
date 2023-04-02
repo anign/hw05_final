bcaa59b1e6ef2415e8d2e101b68a12a7001e5ca0
Anton Ign <i_a_v_86@mail.ru>
2023-04-02 03:33:52 +0300

    Sentry added

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -13,6 +13,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+sentry_sdk.init(
+    dsn="f9db9548d0eb11edb60a76176438fab0",
+    integrations=[DjangoIntegration()],
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
